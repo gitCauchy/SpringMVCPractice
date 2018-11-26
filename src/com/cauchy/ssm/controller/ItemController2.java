67edd7be6312bcd41b46e40eaef535fce184cb0f
gitCauchy <mailCauchy@gmail.com>
2018-11-26 11:12:22 +0800

    finish fundamental code

---
+++ src/com/cauchy/ssm/controller/ItemController2.java
@@ -0,0 +1,38 @@
+package com.cauchy.ssm.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.HttpRequestHandler;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import com.cauchy.ssm.po.Item;
+
+public class ItemController2 implements HttpRequestHandler{
+	
+	public void handleRequest(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException{
+		// 调用service来查询数据库，查询商品列表，这里使用静态数据
+		List<Item> itemList = new ArrayList<Item>();
+		Item item1 = new Item();
+		item1.setName("Cellphone");
+		item1.setDetail("a beautiful cellphone");
+		item1.setPrice(2000f);
+		Item item2 = new Item();
+		item2.setName("Computer");
+		item2.setDetail("a handsome computer");
+		item2.setPrice(5000f);
+		itemList.add(item1);
+		itemList.add(item2);
+		// 设置模型数据
+		request.setAttribute("itemList", itemList);
+		// 设置转发视图：
+		request.getRequestDispatcher("/WEB-INF/jsp/item/itemlist.jsp").forward(request, response);
+	}
+}
---
+++ src/com/cauchy/ssm/po/Item.java
@@ -0,0 +1,50 @@
+package com.cauchy.ssm.po;
+
+import java.util.Date;
+
+public class Item {
+	private Integer id;
+	private String name;
+	private String detail;
+	private float price;
+	private String pic;
+	private Date createTime;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDetail() {
+		return detail;
+	}
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getPic() {
+		return pic;
+	}
+	public void setPic(String pic) {
+		this.pic = pic;
+	}
+	public Date getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+	
+	
+}
